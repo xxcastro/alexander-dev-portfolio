6c70882d33f4d0637cc1ed696f7e95725bedaf54
xxcastro <alexgavilanez672@gmail.com>
2025-10-30 17:43:22 +0100

    Tercer commit: supuesta solucion , ya me funciona los estilos de tailwindcss

--- frontend/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// frontend/postcss.config.mjs
-export default {
-  plugins: {
-    // CAMBIO CLAVE: Usa el paquete @tailwindcss/postcss en lugar del 'tailwindcss' directo
-    '@tailwindcss/postcss': {}, 
-    autoprefixer: {},
-  },
-};
